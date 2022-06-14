5abff805fe479a189dde8c0815ae1dc107aa41e6
Mobeen Abdullah <mobeenabdullah@gmail.com>
2022-06-15 04:07:35 +0500

    Setup About and Error pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni quas modi
+        vel! Numquam sit eos repudiandae fuga hic commodi, cupiditate expedita,
+        assumenda quae enim, laudantium perspiciatis magni laborum omnis velit?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
